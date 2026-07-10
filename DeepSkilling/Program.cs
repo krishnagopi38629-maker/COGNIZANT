2294d6d12aa994fae638dd68a886a4a1a4ebc5e8
Jangili Gopi Krishna <krishnagopi38629@gmail.com>
2026-07-10 09:42:19 +0530

    Add files via upload

---
+++ DeepSkilling/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace MVCPatternExample
+{
+    // Model
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Grade { get; set; }
+    }
+
+    // View
+    public class StudentView
+    {
+        public void DisplayStudentDetails(Student student)
+        {
+            Console.WriteLine("Student Details");
+            Console.WriteLine("----------------");
+            Console.WriteLine($"ID    : {student.Id}");
+            Console.WriteLine($"Name  : {student.Name}");
+            Console.WriteLine($"Grade : {student.Grade}");
+        }
+    }
+
+    // Controller
+    public class StudentController
+    {
+        private Student model;
+        private StudentView view;
+
+        public StudentController(Student model, StudentView view)
+        {
+            this.model = model;
+            this.view = view;
+        }
+
+        public void SetStudentName(string name)
+        {
+            model.Name = name;
+        }
+
+        public void SetStudentGrade(string grade)
+        {
+            model.Grade = grade;
+        }
+
+        public void UpdateView()
+        {
+            view.DisplayStudentDetails(model);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student
+            {
+                Id = 101,
+                Name = "Hemanth",
+                Grade = "A"
+            };
+
+            StudentView view = new StudentView();
+
+            StudentController controller = new StudentController(student, view);
+
+            Console.WriteLine("Initial Student Details:");
+            controller.UpdateView();
+
+            Console.WriteLine();
+
+            controller.SetStudentName("Rahul");
+            controller.SetStudentGrade("A+");
+
+            Console.WriteLine("Updated Student Details:");
+            controller.UpdateView();
+        }
+    }
+}
